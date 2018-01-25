5978cca722d46986da30d1c6a8d4f188cad84b3b
Ethan O'Brien <epobrien@ucsc.edu>
2018-01-24 17:57:56 -0800

    removed virtualenv and added documentation folder containing documents submitted so far

--- TESTENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ethanobrien/Documents/schoolwork/w18/CS115/project/TESTENV/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
